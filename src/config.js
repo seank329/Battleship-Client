b715faf47bd96e83e52bf6b2ca71453b43b82098
seank329 <seankamran@gmail.com>
2020-03-05 11:02:34 -0500

    Changed API endpoint

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 //'https://capstone3-battleship-server.herokuapp.com'
 
 const config = {
-    API_ENDPOINT:   'http://localhost:8000',
+    API_ENDPOINT:   'https://boiling-sands-36862.herokuapp.com',
 }
 
 export default config;
